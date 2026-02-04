54e02ecae145078889b5c412d65da4ab81f4e71d
ErikING-Chile <efloresarpe>
2026-02-04 13:19:38 -0300

    fix(auth): pass entire updated request object to middleware response to ensure cookie propagation

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -18,11 +18,11 @@ export async function updateSession(request: NextRequest) {
                 },
                 setAll(cookiesToSet) {
                     cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
+
                     response = NextResponse.next({
-                        request: {
-                            headers: request.headers,
-                        },
+                        request,
                     })
+
                     cookiesToSet.forEach(({ name, value, options }) =>
                         response.cookies.set(name, value, options)
                     )
